5bc6597fae6b342c8cb8cbd44c80deaf927547e2
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-07-31 10:54:34 +0530

    Adding server files

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const serverConfig = require('./configs/server.config');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
+const bcrypt = require('bcrypt');
+const User = require('./models/user.model');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+db.on("error",()=>{
+    console.log("Error while connecting to MongoDB");
+});
+db.once("open", ()=>{
+    console.log("Connected to mongoDB");
+    init();
+});
+
+async function init(){
+    try{
+        //drop the collections
+        await User.collection.drop();
+
+        const user = await User.create({
+            name : "admin",
+            userId : "admin",
+            password : bcrypt.hashSync("test1234",8),
+            email : "admin123@gmail.com",
+            userType : "ADMIN"
+        });
+
+        console.log(user);
+    }catch(err){
+        console.log("err in db initialization , " + err.message);
+
+    }
+}
+
+require('./routes/auth.route')(app);
+require('./routes/user.route')(app);
+require('./routes/ticket.route')(app);
+
+app.listen(serverConfig.PORT,()=>{
+    console.log("Started the server on the PORT number : ", serverConfig.PORT);
+});
