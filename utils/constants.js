620f00c549e57548fcc8d8f4a10d51293f412eea
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-07-31 10:53:58 +0530

    adding contants objectConverter

---
+++ utils/constants.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    userType : {
+        customer : "CUSTOMER",
+        admin : "ADMIN",
+        engineer : "ENGINEER"
+    },
+
+    userStatus : {
+        pending : "PENDING",
+        approved : "APPROVED",
+        rejected : "REJECTED"
+    },
+
+    ticketStatsues : {
+        open : "OPEN",
+        close : "CLOSE",
+        blocked : "BLOCKED"
+    }
+}
